c379ff0921084f7b1948123db5726654ffd839dc
Emy <f.emeric.emy@gmail>
2024-07-31 15:04:59 +0100

    st1tk1 visual representation

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1">
+    <title>Exoplanets</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+          integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
+          crossorigin="anonymous" referrerpolicy="no-referrer"/>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="header">
+    <h1>Exoplanets</h1>
+</div>
+<div class="itemsContainer">
+    <?php
+    $db = new PDO('mysql:host=db; dbname=collector', 'root', 'password');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    $query = $db->prepare('SELECT * FROM `exoplanets`');
+    $query->execute();
+    $exoPlanets = $query->fetchAll();
+    foreach ($exoPlanets as $planet) {
+        echo '<div class="itemCard">';
+        echo '<img src="' . $planet['image_url'] . '" alt="' . $planet['name'] . '">';
+        echo '<h2>' . $planet['name'] . '</h2>';
+        echo '<br>';
+        echo '<p>' . $planet['fact'] . '</p>';
+        echo '<br>';
+        echo '<p>Location: ' . $planet['location'] . '</p>';
+        echo '<p>Type: ' . $planet['type'] . '</p>';
+        echo '<p>Orbital period: : ' . $planet['orbital_period'] . '</p>';
+        echo '<p>Distance: ' . $planet['distance'] . '</p>';
+        echo '</div>';
+    }
+    ?>
+</div>
+<div class="addItemButton">
+    <a href="add_item.php" style="text-decoration: none; color: white;">Add new Exoplanets</a>
+</div>
+</body>
+</html>

17f784639ca06d615bcb59cf3bab10c1162eb80f
Emy <f.emeric.emy@gmail>
2024-09-03 22:48:19 +0200

    data base edited for heroku deploy

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$db = new PDO('mysql:host=db; dbname=collector', 'root', 'password');
+$db = new PDO('mysql:host=w1kr9ijlozl9l79i.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;dbname=by5m6ddmdn7ydny6;charset=utf8mb4', 'mze1772050xpm0xu', 's9z8ndxhj6rq9ta6');
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 ?>
